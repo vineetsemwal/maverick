1a1b03cfbaa7248b926311808e7dee23c2d89e76
Vineet Semwal <vineetsemwal82@gmail.com>
2023-07-11 11:48:19 +0530

    Create proxy_task.md

---
+++ proxy_task.d
@@ -0,0 +1,19 @@
+Create an Original Component that displays data in the below manner
+At a later time create a secure component proxy so that client can only see the data if he is signed in
+
+interface IComponent{
+
+void renders();
+
+}
+
+DashboardComponent implements IComponent{
+void renders(){
+// displays hardcoded data
+}
+}
+
+
+
+
+
