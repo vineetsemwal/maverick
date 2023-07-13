610333d12719f567454f4bb97d7312950e67f429
vineetsemwal <vineetsemwal82@gmail.com>
2023-07-13 15:13:39 +0530

    spread demo

---
+++ frontend/javascript/spread.js
@@ -0,0 +1,8 @@
+let product1={name:"samsung", model:"galaxy g1", category:"phone"};
+
+let product2={...product1, model:"galaxy g2"};
+
+console.log("product1", product1);
+console.log("product2", product2);
+
+
