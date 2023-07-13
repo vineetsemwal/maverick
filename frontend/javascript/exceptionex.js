a4a140e0c323ccb0a3c01f7ff9c7e682a06beed6
vineetsemwal <vineetsemwal82@gmail.com>
2023-07-13 14:29:08 +0530

    exception handling

---
+++ frontend/javascript/exceptionex.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+  if (!num1 || !num2) {
+    throw new Error("num1 is undefined");
+  }
+  return num1+num2;
+
+}
+
+function addUserClient() {
+  try {
+    var result = add(1);
+    console.log("result=" + result);
+  } catch (err) {
+    console.log("exception caught="+err);
+    
+  }
+}
+
+addUserClient();
