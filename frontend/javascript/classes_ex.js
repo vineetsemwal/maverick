d3c8f05f578cb48140dd1eee1ed0b0b0ca48ea96
vineetsemwal <vineetsemwal82@gmail.com>
2023-07-14 10:53:53 +0530

    formatting on class demo

--- frontend/javascript/classes_ex.js
+++ frontend/javascript/classes_ex.js
@@ -1,19 +1,17 @@
-class Employee{
-static employeesCount=0;
-   
-constructor(id,name){
-this.id=id;
-this.name=name;  
-Employee.employeesCount++ ; 
-}
+class Employee {
+  static employeesCount = 0;
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    Employee.employeesCount++;
+  }
 
-display(){
- console.log(this.id+"-"+this.name);   
+  display() {
+    console.log(this.id + "-" + this.name);
+  }
 }
-}
-
-const emp1=new Employee(1,"kailash");
-const emp2=new Employee(2,"digvijay");
+const emp1 = new Employee(1, "kailash");
+const emp2 = new Employee(2, "digvijay");
 emp1.display();
 emp2.display();
-console.log("count="+Employee.employeesCount);
+console.log("count=" + Employee.employeesCount);
