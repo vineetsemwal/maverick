3ea60b6a48ddfca8d0fb03f0688b04b592bf4ca9
vineetsemwal <vineetsemwal82@gmail.com>
2023-07-14 10:51:55 +0530

    class demo

---
+++ frontend/javascript/classes_ex.js
@@ -0,0 +1,19 @@
+class Employee{
+static employeesCount=0;
+   
+constructor(id,name){
+this.id=id;
+this.name=name;  
+Employee.employeesCount++ ; 
+}
+
+display(){
+ console.log(this.id+"-"+this.name);   
+}
+}
+
+const emp1=new Employee(1,"kailash");
+const emp2=new Employee(2,"digvijay");
+emp1.display();
+emp2.display();
+console.log("count="+Employee.employeesCount);
