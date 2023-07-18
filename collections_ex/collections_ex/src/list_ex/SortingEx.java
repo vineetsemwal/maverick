0f29a30944fdf2e93a31455e8f39809124adedb9
vineetsemwal <vineetsemwal82@gmail.com>
2023-07-18 12:15:25 +0530

    sorting multiple fields demo

--- collections_ex/collections_ex/src/list_ex/SortingEx.java
+++ collections_ex/collections_ex/src/list_ex/SortingEx.java
@@ -7,11 +7,18 @@ import java.util.List;
 public class SortingEx {
     public static void main(String[] args) {
         List<Product>list=new ArrayList<>();
+        list.add(new Product("samsung",30000));
         list.add(new Product("samsung",20000));
         list.add(new Product("moto",15000));
         list.add(new Product("iphone",70000));
-        Comparator<Product>ascByPrice=(product1,product2)->product1.price-product2.price;
-        list.sort(ascByPrice);
+        Comparator<Product>ascByNameAndPrice=(product1,product2)-> {
+            boolean equals=product1.name.equals(product2.name);
+            if(!equals){
+                return product1.name.compareTo(product2.name);
+            }
+            return product1.price-product2.price;
+        };
+        list.sort(ascByNameAndPrice);
         System.out.println("list asc order by price="+list);
         Comparator<Product>descByPrice=(product1,product2)->product2.price-product1.price;
         list.sort(descByPrice);
