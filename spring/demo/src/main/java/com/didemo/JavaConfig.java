258493e90ea2818834348d1cccc650a836f18938
vineetsemwal <vineetsemwal82@gmail.com>
2023-07-18 15:41:37 +0530

    propertysource demo

--- spring/demo/src/main/java/com/didemo/JavaConfig.java
+++ spring/demo/src/main/java/com/didemo/JavaConfig.java
@@ -3,7 +3,9 @@ package com.didemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application.properties")
 @ComponentScan("com.didemo")
 @Configuration
 public class JavaConfig {
--- spring/demo/src/main/java/com/didemo/Square.java
+++ spring/demo/src/main/java/com/didemo/Square.java
@@ -1,9 +1,11 @@
 package com.didemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 //@Component
 public class Square implements Shape{
+    @Value("${square.side}")
     private int side;
     public Square(){
     }
