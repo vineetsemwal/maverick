3cba9b910c182600bd97123f8460bd8d2f0a2803
vineetsemwal <vineetsemwal82@gmail.com>
2023-08-08 14:41:54 +0530

    website demo

---
+++ frontend/angular/routing-demo/src/app/obsverableEx.ts
@@ -0,0 +1,4 @@
+import { Observable, of } from "rxjs";
+
+const observable:Observable<string>=of("apple","mango","grapes");
+observable.
---
+++ frontend/angular/website-demo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
