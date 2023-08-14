fc2bf2578c2ca4634bfef6274ac9c385bfc5e30b
vineetsemwal <vineetsemwal82@gmail.com>
2023-08-14 11:41:27 +0530

    props concept demo

--- frontend/reactjs/first-app/src/App.js
+++ frontend/reactjs/first-app/src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import { UserDetailsComponent } from './UserDetails';
+import { Component } from "react";
+import "./App.css";
+import UserDetailsComponent from "./UserDetailsComponent";
 /*
 React.createElement(
   'div',{},
   '<h1>Main Component</h1>');
 */
-function App() {
-const  title="hello";
-return (<div>
-    <h1>Main Title is {title} </h1>
-    3+5 value ={3+5}
-<UserDetailsComponent/>
+class App extends Component {
+  title = "hello";
 
-    </div>
-  );
+   user1={name:"mani", age:26};
+   user2={name:"manju", age:23};
 
+  giveTitle() {
+    return "Welcome";
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Main Title is {this.title} </h1>
+        Title is {this.giveTitle()}
+
+        <UserDetailsComponent user={this.user1} dept="IT" />
+        <UserDetailsComponent user={this.user2} dept="HR"/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/reactjs/first-app/src/UserDetailsComponent.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+ class UserDetailsComponent extends Component {
+ 
+    constructor(props){
+        super(props);
+    }
+
+
+  render() {
+    const userObj=this.props.user;
+    return (
+      <div>
+        <h1>User Details Component</h1>
+        Name is {userObj.name}, age is {userObj.age}
+        <br/>
+         Department is {this.props.dept}
+      </div>
+    );
+  }
+}
+
+export default UserDetailsComponent;
